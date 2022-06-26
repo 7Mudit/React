40fa6e4a84c77e252c3fa5db13bea8beed929bd9
Mudit Kapoor <muditk441@gmail.com>
2022-06-26 16:03:25 +0530

    Home component updated

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Menu from "./components/MenuComponent";
 import "./App.css";
 import {DISHES} from './shared/dishes'
 import {COMMENTS} from './shared/Comments'
+import {LEADERS} from './shared/leaders'
+import {PROMOTIONS} from './shared/promotions'
 import Header from './components/HeaderComponent';
 import Footer from './components/FooterComponent';
 import {BrowserRouter,Routes,Route} from'react-router-dom'
 import Home from './components/HomeComponent';
+import Contact from './components/ContactComponent';
 
 class App extends Component{
   constructor(props) {
@@ -16,7 +19,10 @@ class App extends Component{
 
     this.state={
       dishes:DISHES,
-      comments:COMMENTS
+      comments:COMMENTS,
+      leaders:LEADERS,
+      promotions:PROMOTIONS
+
     }
   }
   render(){
@@ -27,7 +33,8 @@ class App extends Component{
 
     <Routes>
       <Route path='/menu' element={<Menu dishes={this.state.dishes} comments={this.state.comments}/>}/>
-      <Route path='/home' element={<Home/>}/>
+      <Route exact path='/home' element={<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}leader={this.state.leaders.filter((leader) => leader.featured)[0]} promotion={this.state.promotions.filter((promo) => promo.featured)[0]}/>}/>
+      <Route exact path='/contact' element={<Contact/>}/>
     </Routes>
       <Footer/>
     </BrowserRouter>
