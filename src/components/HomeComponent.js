e133e194c3df0c1b5371f7d0eadc79cdd7a0312c
Mudit Kapoor <muditk441@gmail.com>
2022-06-28 10:37:35 +0530

    react animation components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -7,9 +7,9 @@ import {
   CardTitle,
   CardSubtitle,
 } from "reactstrap";
-import {baseUrl} from '../shared/baseUrl'
+import { baseUrl } from "../shared/baseUrl";
 import { Loading } from "./LoadingComponent";
-
+import { FadeTransform } from "react-animation-components";
 
 function RenderCard({ item, isLoading, errMess }) {
   if (isLoading) {
@@ -24,16 +24,23 @@ function RenderCard({ item, isLoading, errMess }) {
     );
   } else
     return (
-      <Card>
-        <CardImg src={baseUrl+item.image} alt={item.name} />
-        <CardBody>
-          <CardTitle>{item.name}</CardTitle>
-          {item.designation ? (
-            <CardSubtitle>{item.designation}</CardSubtitle>
-          ) : null}
-          <CardText>{item.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: "scale(0.5) translateY(-50%)",
+        }}
+      >
+        <Card>
+          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? (
+              <CardSubtitle>{item.designation}</CardSubtitle>
+            ) : null}
+            <CardText>{item.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
     );
 }
 
@@ -49,8 +56,11 @@ function Home(props) {
           />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.promotion}isLoading={props.promosLoading}
-            errMess={props.promosErrMess} />
+          <RenderCard
+            item={props.promotion}
+            isLoading={props.promosLoading}
+            errMess={props.promosErrMess}
+          />
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard item={props.leader} />
